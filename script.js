7e6ec82476b1a58c5205ee959e2bdf5245ce744f
Red-de-apoyo-gob-mx <sanchez.v.brayan@gmail.com>
2025-07-23 17:27:34 -0600

    Actualización de contenido y diseño

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const videos = [
 
     // Botón WhatsApp
     const btnWhats = document.createElement('a');
-    btnWhats.href = 'https://wa.me/5215516181754';
+    btnWhats.href = 'https://wa.me/5215513655749';
     btnWhats.target = '_blank';
     btnWhats.className = 'btn btn-contacto';
     btnWhats.innerHTML = '<i class="fab fa-whatsapp"></i>';
